15e71c348c1e5a8546122575225c8fe46dc66c6c
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2025-12-18 21:46:47 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Intergalactic-Photos
+## Galaxy Photos
+
+Development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Page = () => {
+  return (
+    <div className="">
+      <h1>Galaxy Photos</h1>
+      <section className="container max-w-5xl mx-auto"></section>
+      
+    </div>
+  );
+};
+
+export default Page;
