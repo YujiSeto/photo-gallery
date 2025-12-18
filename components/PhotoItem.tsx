42fa1c09b35419fe3b50e63f15a48f31f497ba7d
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2025-12-18 23:47:26 +0900

    fix: correct image paths for GitHub Pages deployment

--- components/PhotoItem.tsx
+++ components/PhotoItem.tsx
@@ -5,10 +5,12 @@ type Props = {
   onClick: () => void;
 };
 
+const basePath = process.env.NODE_ENV === "production" ? "/photo-gallery" : "";
+
 export const PhotoItem = ({ photo, onClick }: Props) => {
   return (
     <div onClick={onClick} className="cursor-pointer hover:opacity-80">
-      <img src={`/assets/${photo.url}`} alt="" className="w-full" />
+      <img src={`${basePath}/assets/${photo.url}`} alt="" className="w-full" />
     </div>
   );
 };
